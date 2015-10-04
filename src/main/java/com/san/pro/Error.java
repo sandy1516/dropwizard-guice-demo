b3972a389dbb070ff6cd4108dc350fe734b19337
sandy <sandeep.s1605@gmail.com>
2015-10-05 02:35:08 +0530

    updated

---
+++ src/main/java/com/san/pro/Error.java
@@ -0,0 +1,17 @@
+package com.san.pro;
+
+/**
+ * Created by Administrator on 05-10-2015.
+ */
+public class Error {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/san/pro/resource/UserResource.java
+++ src/main/java/com/san/pro/resource/UserResource.java
@@ -4,6 +4,8 @@
 
 package com.san.pro.resource;
 
+import com.san.pro.*;
+import com.san.pro.Error;
 import com.san.pro.dao.UserDao;
 import com.san.pro.model.User;
 
@@ -11,6 +13,8 @@ import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.text.NumberFormat;
 
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
@@ -27,17 +31,32 @@ public class UserResource {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public User getUser(@PathParam("id") long id)
+    public Response getUser(@PathParam("id") String stringId)
     {
-        return userDao.getById(id);
+        try {
+            Long id = Long.parseLong(stringId);
+            User user = userDao.getById(id);
+            if(user == null) {
+                com.san.pro.Error error = new Error();
+                error.setMessage("Entity not found");
+                return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+                //return Response.status(Response.Status.NOT_FOUND).entity("Entity not found for id: " + id).build();
+            }
+            return Response.status(Response.Status.OK).entity(user).build();
+        } catch(NumberFormatException ex) {
+            com.san.pro.Error error = new Error();
+            error.setMessage("Invalid ID");
+            return Response.status(Response.Status.BAD_REQUEST).entity(error).build();
+        }
     }
 
     @POST
     @Path("create")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public User addUser(@Valid User user) {
-        return userDao.save(user);
+    public Response addUser(@Valid User user) {
+
+        return Response.status(Response.Status.CREATED).entity(user).build();
     }
 
 }
